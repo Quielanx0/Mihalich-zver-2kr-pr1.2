f4ce30a21b6852f3fac1be1b0c9336395f6dd25a
Александр Исаев <Quielanx0@outlook.com>
2021-09-09 11:11:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mihalich_zver
+{
+    SByte a = 0;
+    Byte b = 0;
+    Int16 с = 0;
+    Int32 d = 0;
+    Int64 e = 0;
+    string s = "";
+    Exception ex = new Exception();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
